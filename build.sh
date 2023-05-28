1233f1dd97028807b2393ba2defa9dac64e8b554
Mathieu Stiénon <stienon@gmail.com>
2023-05-28 14:27:33 +0200

    modified:   build.sh 	renamed:    NSF_4-Color_bitmap_Logo.png -> hirsutes2023/NSF_4-Color_bitmap_Logo.png 	renamed:    PS_HOR_RGB_2C.png -> hirsutes2023/PS_HOR_RGB_2C.png 	renamed:    gap2023.md -> hirsutes2023/gap2023.md 	renamed:    gap2023abstracts.md -> hirsutes2023/gap2023abstracts.md 	renamed:    ihp.png -> hirsutes2023/ihp.png 	renamed:    july.md -> hirsutes2023/july.md 	renamed:    june.md -> hirsutes2023/june.md 	renamed:    uzh.png -> hirsutes2023/uzh.png

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 pandoc -s --metadata pagetitle="Mathieu Stiénon" -c GitHub2.css index.md -o index.html
 pandoc -s --metadata pagetitle="GAP" -c GitHub2.css gap.md -o gap.html
-#pandoc -s --metadata pagetitle="HIRSUTES 2023" -c GitHub2.css hirsutes2023.md -o hirsutes2023.html
-pandoc -s --metadata pagetitle="GAP 2023 — Homotopy Algebras and Higher Structures" -c GitHub2.css gap2023.md -o gap2023.html
-pandoc -s --metadata pagetitle="GAP 2023 — Schedule and Abstracts" -c GitHub2.css gap2023abstracts.md -o gap2023abstracts.html
-pandoc -s --metadata pagetitle="Higher Structures in Enumerative Geometry" -c GitHub2.css june.md -o june.html
-pandoc -s --metadata pagetitle="Dg-manifolds in Geometry and Physics" -c GitHub2.css july.md -o july.html
+#pandoc -s --metadata pagetitle="HIRSUTES 2023" -c GitHub2.css hirsutes2023/hirsutes2023.md -o hirsutes2023/hirsutes2023.html
+pandoc -s --metadata pagetitle="GAP 2023 — Homotopy Algebras and Higher Structures" -c GitHub2.css hirsutes2023/gap2023.md -o hirsutes2023/gap2023.html
+pandoc -s --metadata pagetitle="GAP 2023 — Schedule and Abstracts" -c GitHub2.css hirsutes2023/gap2023abstracts.md -o hirsutes2023/gap2023abstracts.html
+pandoc -s --metadata pagetitle="Higher Structures in Enumerative Geometry" -c GitHub2.css hirsutes2023/june.md -o hirsutes2023/june.html
+pandoc -s --metadata pagetitle="Dg-manifolds in Geometry and Physics" -c GitHub2.css hirsutes2023/july.md -o hirsutes2023/july.html
+
